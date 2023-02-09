1882b701328f38f336702b9e80095a1dbdf6085d
JoKronk <JoKronkvist@gmail.com>
2023-02-09 16:20:25 +0200

    Add snackbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,23 +7,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 import { NavBoardComponent } from './nav-board/nav-board.component';
 import { LobbyComponent } from './lobby/lobby.component';
+import { SnackbarComponent } from './snackbar/snackbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBoardComponent,
-    LobbyComponent
+    LobbyComponent,
+    SnackbarComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSidenavModule,
-    MatButtonModule
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
