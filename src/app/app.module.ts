bdc414c07970a341f2ddd0613e5d82e5cb753cfa
JoKronk <JoKronkvist@gmail.com>
2023-03-07 13:38:48 +0200

    Remove online players component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import { NavBoardComponent } from './nav-board/nav-board.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
 import { LobbyComponent } from './lobby/lobby.component';
 import { SnackbarComponent } from './snackbar/snackbar.component';
-import { OnlinePlayersComponent } from './online-players/online-players.component';
 import { CreateRunComponent } from './dialogs/create-run/create-run.component';
 import { SetPathComponent } from './dialogs/set-path/set-path.component';
 import { RunComponent } from './run/run.component';
@@ -33,7 +32,6 @@ import { CloseScreenComponent } from './close-screen/close-screen.component';
     StartScreenComponent,
     LobbyComponent,
     SnackbarComponent,
-    OnlinePlayersComponent,
     CreateRunComponent,
     SetPathComponent,
     RunComponent,
