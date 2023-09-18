b0386e46dc99379af07b479a7ccf0dce03f4ec59
JoKronk <JoKronkvist@gmail.com>
2023-09-19 01:49:50 +0300

    Fix import in leaderboard spec

--- src/app/leaderboard/leaderboard.component.spec.ts
+++ src/app/leaderboard/leaderboard.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LeaderboardComponent } from './leaderboard.component';
 
-import LeaderboardComponent from './leaderboard.component';
 
 describe('LeaderboardComponent', () => {
   let component: LeaderboardComponent;
