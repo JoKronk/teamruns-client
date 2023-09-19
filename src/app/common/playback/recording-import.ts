859143c8a3ff7351bf23f0e8dbf2df3307dd26c6
JoKronk <JoKronkvist@gmail.com>
2023-09-19 22:30:43 +0300

    Remove unused properties from record import

--- src/app/common/playback/recording-import.ts
+++ src/app/common/playback/recording-import.ts
@@ -1,23 +1,10 @@
 export class RecordingImport {
     
     name: string;
-    size: string;
     path: string;
 
     constructor(file: any) {
         this.name = file.name.slice(0, -5);
-        this.size = this.formatBytes(file.size);
         this.path = file.path;
-    }
-
-    private formatBytes(bytes: number, decimals: number = 2) {
-      if (bytes === 0) {
-        return '0 Bytes';
-      }
-      const k = 1024;
-      const dm = decimals <= 0 ? 0 : decimals || 2;
-      const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-      const i = Math.floor(Math.log(bytes) / Math.log(k));
-      return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
     }
 }
