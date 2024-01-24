6365f107d7392bb180645e3df9d666cf044cae58
JoKronk <JoKronkvist@gmail.com>
2024-01-24 17:55:21 +0200

    Fix game settings not being set correctly

--- src/app/common/socket/game-settings.ts
+++ src/app/common/socket/game-settings.ts
@@ -5,33 +5,33 @@ import { RunMode } from "../run/run-mode";
 export class GameSettings {
     category: CategoryOption;
     mode: RunMode;
-    requireSameLevel: boolean;
-    allowSoloHubZoomers: boolean;
-    noLTS: boolean;
+    requireSameLevel: number;
+    allowSoloHubZoomers: number;
+    noLTS: number;
     citadelSkip: CitadelOption;
-    enablePvp: boolean;
-    freeForAll: boolean;
+    enablePvp: number;
+    freeForAll: number;
 
     constructor (runData: RunData | undefined) {
         if (runData) {
             this.category = runData.category;
             this.mode = runData.mode;
-            this.requireSameLevel = runData.requireSameLevel;
-            this.allowSoloHubZoomers = runData.allowSoloHubZoomers;
-            this.noLTS = runData.noLTS;
+            this.requireSameLevel = runData.requireSameLevel ? 1 : 0;;
+            this.allowSoloHubZoomers = runData.allowSoloHubZoomers ? 1 : 0;;
+            this.noLTS = runData.noLTS ? 1 : 0;;
             this.citadelSkip = runData.citadelSkip;
-            this.enablePvp = runData.enablePvp;
-            this.freeForAll = runData.teams === 1 && runData.mode === RunMode.Lockout;
+            this.enablePvp = runData.enablePvp ? 1 : 0;;
+            this.freeForAll = runData.teams === 1 && runData.mode === RunMode.Lockout ? 1 : 0;;
         }
         else {
             this.category = CategoryOption.NoLts;
             this.mode = RunMode.Speedrun;
-            this.requireSameLevel = false;
-            this.allowSoloHubZoomers = true;
-            this.noLTS = false;
+            this.requireSameLevel = 0;
+            this.allowSoloHubZoomers = 1;
+            this.noLTS = 0;
             this.citadelSkip = CitadelOption.Normal;
-            this.enablePvp = false;
-            this.freeForAll = false;
+            this.enablePvp = 0;
+            this.freeForAll = 0;
         }
     }
 }
